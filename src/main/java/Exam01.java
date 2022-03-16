cef6d6572c70b3aded9932ca26e3c9342c51a958
shin686com <sgs686@naver.com>
2022-03-16 11:33:22 +0900

    20220316_Test

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,9 @@
+
+public class Exam01 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("Java입니다");
+        System.out.println("ㄴ ㅏ     ㅈ ㅏ");
+        System.out.println(" ㅁ");
+    }
+}
