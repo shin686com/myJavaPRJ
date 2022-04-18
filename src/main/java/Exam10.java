2af3211a2a56de52eaf4017bbcf615030770ae6c
shin686com <sgs686@naver.com>
2022-04-18 15:08:46 +0900

    2022 04 18 커밋

---
+++ src/main/java/Exam10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exam10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i = 1; i <= n ; i++) {
+            for(int j = 0; j < i; j++)
+                System.out.print("*");
+            for(int j = 0; j < (n-i)*2; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        for(int i = 1; i <= n ; i++) {
+            for(int j = 0; j < n-i; j++)
+                System.out.print("*");
+            for(int j = 0; j < i*2; j++)
+                System.out.print(" ");
+            for(int j = 0; j < n-i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
